d20426555769ff3610d6768db3d4b29f6f2ef6bd
Jin Zhou <jzhou@email.arizona.edu>
2016-01-16 00:35:12 -0700

    update code extractHsq.sh

--- codebase/extractHsq.sh
+++ codebase/extractHsq.sh
@@ -6,11 +6,40 @@
 #  Created by Jin Zhou on 1/14/16.
 #
 
-for entry in "."/*.hsq
+# define phenotypes and extract phenotype names
+# first two colums are FID and IID
+phenotypefile=CG10kNHWRes4Subtyping.txt
+names=$(head -n 1 ../datasets/$phenotypefile)
+namesarray=($names)
+len="${#namesarray[@]}"
+phenotypes=$((len-2))
+
+
+cat > estHsq.txt << EOF
+EOF
+
+for i in $(seq $phenotypes)
 do
-hsq=$(grep -r 'V(G)'  $entry)
-x='./'
-hsq=${hsq//$x/''}
-n=$(grep -A 1 'Pval' $entry | grep  'n')
-echo $hsq" "$n
+echo $i
+# get the phenotypes name
+pi1=$((i+1))
+outpi1=${namesarray[$pi1]}
+echo $outpi1
+hsqfile=${outpi1}.hsq
+gvar=$(grep -r 'V(G)'  $hsqfile)
+n=$(grep -A 1 'Pval' $hsqfile | grep  'n')
+echo ${outpi1}" "$gvar" "$n >> estHsq.txt
+ii=$((i+1))
+    for j in $(seq $ii $phenotypes)
+    do
+    echo $j
+    pi2=$((j+1))
+    outpi2=${namesarray[$pi2]}
+    hsqfile=$outpi1$outpi2".hsq"
+    gvar=$(grep -r 'C(G)_tr12'  $hsqfile)
+    n=$(grep -A 1 'Pval' $hsqfile | grep  'n')
+    echo $outpi1$outpi2" "$gvar" "$n >> estHsq.txt
+    done
 done
+
+
